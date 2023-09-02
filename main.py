41de9c92547642a725ec496e652c6a35f873e2b7
aStevee <77806784+astevenn@users.noreply.github.com>
2023-09-02 16:39:41 +0200

    data strutures, set new description project and files created

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from API_integrations import calculations
+from API_integrations import graphic_deign
+import os,asyncio, aiofiles
+
+
+def get_file(file):
+    with open(os.getcwd() + f"/{file}","r") as f:
+        return f.read() 
+
 
 app = FastAPI(
-    description = """ 
-        This is an application using FAST API that allows users to perform basic statistical calculations on numerical datasets. 
-        The application will accept data in the form of lists of numbers and provide statistical results such as mean, median, 
-        standard deviation, and sum of the entered numbers using the NumPy library.""",
+    description = get_file("README.md"),
     version="beta 0.0.1",
-    docs_url="/documentation"
-    )
+    docs_url="/documentation",
+)
 
 
-@app.get("/")
-def read_root():
+"""Application start here"""
+
+
+@app.get("/", description="Simple message where we can see that the server is actually working")
+async def root():
     return {"result":"the server is actually working!"}
+
 
 
 
